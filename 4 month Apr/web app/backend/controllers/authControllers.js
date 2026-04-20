4ff5a0d45063e5b3939302c341289b25aca08c88
nikitajay <ajaynikita12@gmail.com>
2026-04-20 20:11:49 +0530

    done

--- 4 month Apr/web app/backend/controllers/authControllers.js
+++ 4 month Apr/web app/backend/controllers/authControllers.js
@@ -1,3 +1,83 @@
-//login Apl
-//Register users Api
 
+//Register users Api
+const jwt = require("jsonwebtoken")
+
+const Users = require("../Models/UserModel")
+
+const register = async (req, res) => {
+    try {
+        const { name, email, pass, city, mobileNO } = req.body
+
+        const existingUser = await Users.findOne({ email : email })
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists with this email"
+            })
+        }
+
+        const user = await Users.create({
+            name,
+            email,
+            city,
+            mobileNO,
+            password: pass
+        })
+        const token = jwt.sign({ userId: user._id, email: email },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "30d" })
+
+        res.status(201).json({
+            message: "Register Successfull",
+            data: user,
+            token: token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            message: "Register Failed",
+            error: error.message
+
+        })
+    }
+}
+
+
+//login Apl
+
+const login = async (req, res) => {
+    try {
+        const { email, pass } = req.body
+
+        const user = await Users.findOne({ email, password: pass })
+
+        const token = jwt.sign({ userId: user._id, email: email },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "30d" })
+
+
+
+        if (user) {
+            res.status(201).json({
+                message: "Login Successful",
+                token: token           
+            })
+        } else {
+            res.status(404).json({
+                message: "Invalid Credentials",
+            })
+        }
+
+
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(404).json({
+            message: "Login Failed",
+            error: error.message
+        })
+
+    }
+}
+module.exports = { login, register }
